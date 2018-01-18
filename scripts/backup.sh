43522a1dc607ce61ffb840359c013d6a19c4e456
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2018-01-18 11:41:34 +0530

    updated scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash                                                                                                                                           
+
+DB_NAME="lds"
+DB_PASS="root"
+
+OUT_FILE_NAME="ldsbackup"
+VERSION="v3.0.0"
+OUT_FILE="$OUT_FILE_NAME-$VERSION.tar"
+
+
+# take the dump                                                                                                                                       
+mysqldump -u root -p$DB_PASS $DB_NAME > $DB_NAME.sql
+if [ $? -ne 0 ]; then
+  echo "Error: Something went wrong while taking db dump."
+  echo "Aborting backup."
+  exit 1;
+fi
+
+
+tar -cvf $OUT_FILE $DB_NAME.sql
+
+echo "Backup successful."
+exit 0;
+
